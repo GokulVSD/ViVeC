64a20921240af792713575e50db6ad669b712b96
Gokul Vasudeva <gokul.vasda@gmail.com>
2023-09-07 09:51:14 -0700

    Finished task 2

---
+++ Code/task_2.py
@@ -0,0 +1,100 @@
+# Implement a program which extracts and stores feature descriptors
+# for all the images in the data set.
+
+# WARNING: Running this script will overwrite the contents of
+# <git_root>/Outputs/
+
+
+import os
+
+import torch
+from torchvision.datasets import Caltech101
+from torchvision.transforms import Resize, Grayscale, transforms
+
+from feature_extractors.color_moments import get_color_vector
+from feature_extractors.HOG import get_hog_vector
+from feature_extractors.resnet_50 import get_resnet50_feature_vectors
+from utils.utilities import partition_to_grid
+
+
+print("Downloading dataset if not present.")
+
+dataset = Caltech101(root=os.path.abspath(os.path.join(os.path.dirname( __file__ ))), download=True)
+
+print(dataset, "\n")
+
+color_tensors = []
+hog_tensors = []
+avgpool_tensors = []
+layer3_tensors = []
+fc1000_tensors = []
+
+for i in range(len(dataset)):
+    print(f"Processing image {i+1}/{len(dataset)} \t ({int(100*(i+1)/len(dataset))} %)", end='\r')
+
+    image = dataset[i][0]
+
+    # Skip images which are not RGB
+    if image.mode != 'RGB':
+        continue
+
+    # Resize image to 300x100.
+    image300x100 = Resize(size=(100, 300))(image)
+
+    # Partition image into 10x10 grid.
+    grid = partition_to_grid(image300x100, num_rows=10, num_cols=10, hor_pixels=30, ver_pixels=10)
+
+    # Compute color moments vector.
+    color_tensors.append(get_color_vector(grid))
+
+    # Convert image to grayscale.
+    gs_image = Grayscale()(image300x100)
+
+    # Partition grayscale image into 10x10 grid.
+    gs_grid = partition_to_grid(gs_image, num_rows=10, num_cols=10, hor_pixels=30, ver_pixels=10)
+
+    # Compute HOG moments vector.
+    hog_tensors.append(get_hog_vector(gs_grid))
+
+    # Resize original image to 244x244.
+    image224x224 = Resize(size=(224, 224))(image)
+
+    # Convert to float tensor.
+    img_tensor = transforms.Compose([transforms.ToTensor()])(image224x224)
+
+    # Extract features from ResNet-50.
+    avgpool_vector, layer3_vector, fc1000_vector = get_resnet50_feature_vectors(img_tensor)
+
+    avgpool_tensors.append(avgpool_vector)
+    layer3_tensors.append(layer3_vector)
+    fc1000_tensors.append(fc1000_vector)
+
+
+outputs_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'Outputs'))
+
+print("\n\nSaving color feature descriptors to '<git_root>/Outputs/color_tensors.pt'")
+
+torch.save(torch.stack(color_tensors), os.path.join(outputs_dir, 'color_tensors.pt'))
+
+print("\nSaving HOG feature descriptors to '<git_root>/Outputs/hog_tensors.pt'")
+
+torch.save(torch.stack(hog_tensors), os.path.join(outputs_dir, 'hog_tensors.pt\n'))
+
+print("\nSaving ResNet-50 AvgPool feature descriptors to '<git_root>/Outputs/avgpool_tensors.pt'")
+
+torch.save(torch.stack(avgpool_tensors), os.path.join(outputs_dir, 'avgpool_tensors.pt\n'))
+
+print("\nSaving ResNet-50 Layer3 feature descriptors to '<git_root>/Outputs/layer3_tensors.pt'")
+
+torch.save(torch.stack(layer3_tensors), os.path.join(outputs_dir, 'layer3_tensors.pt\n'))
+
+print("\nSaving ResNet-50 Fc1000 feature descriptors to '<git_root>/Outputs/fc1000_tensors.pt'")
+
+torch.save(torch.stack(fc1000_tensors), os.path.join(outputs_dir, 'fc1000_tensors.pt\n'))
+
+print(
+"""
+> Feature vectors for all images are stacked into one tensor per descriptor type.
+> This tensor is saved in binary form, the first dim index is the image ID.
+"""
+)
